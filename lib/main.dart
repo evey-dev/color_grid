43212f7db11fe16ea53623bb9f2b7d38afb08e78
Evey <evelyn.paskhaver@gmail.com>
2022-05-22 15:36:15 -0400

    new method of color mixing

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
-import 'dart:developer';
+import 'dart:developer' as Dev;
+import 'dart:math';
 
 import 'package:color_grid/tile.dart';
 import 'package:flutter/services.dart';
@@ -25,180 +26,63 @@ class _MyAppState extends State<MyApp> {
   final int gridHeight = 5;
   final int gridWidth = 5;
   late List<Tile> tiles;
+  final List<Tile> userTiles = [];
   late List<DragItem> tilesWrapper;
-  late List<ValueNotifier<bool>> dots;
 
   ValueNotifier<int> selected = ValueNotifier<int>(-1);
-
 
   void setSelected(int index) {
     if (selected.value!=-1) {
       tiles[selected.value].selected.value = false;
     }
-    selected.value = index;
-    if (selected.value!=-1) {
+    if (selected.value != index) {
+      selected.value = index;
       tiles[index].selected.value = true;
     }
-    dots[index].value = true;
+    else {
+      selected.value = -1;
+      tiles[index].selected.value = false;
+    }
   }
 
-  // col: index % gridWidth
-  // row: index ~/ gridHeight
-
-  void calculateGrid() {
-    for (int i = 0; i < gridWidth * gridHeight; i++) {
-      tiles[i].autoColor.value = Colors.transparent;
+  void addToUserTiles(int index, bool add) {
+    if (add) {
+      userTiles.add(tiles[index]);
+    } else {
+      userTiles.remove(tiles[index]);
     }
-    for (int i = 0; i < gridWidth * gridHeight; i++) {
-      if (tiles[i].userColor.value != Colors.transparent) {
-        continue;
-      }
-      int dUp = distanceToClosestInclude(i,1);
-      int dDown = distanceToClosestInclude(i,2);
-      int dRight = distanceToClosestInclude(i,3);
-      int dLeft = distanceToClosestInclude(i,4);
-      double p = 1/(1/dUp + 1/dDown + 1/dRight + 1/dLeft);;
-      if (dUp < gridHeight && dDown < gridHeight && dRight < gridWidth && dLeft < gridWidth) {
-        // var R = (tiles[i-gridHeight*dUp].userColor.value == Colors.transparent ? tiles[i-gridHeight*dUp].autoColor.value.red * p/dUp : tiles[i-gridHeight*dUp].userColor.value.red * p/dUp)
-        //       + (tiles[i+gridHeight*dDown].userColor.value == Colors.transparent ? tiles[i+gridHeight*dDown].autoColor.value.red * p/dDown : tiles[i+gridHeight*dDown].userColor.value.red * p/dDown)
-        //       + (tiles[i+dRight].userColor.value == Colors.transparent ? tiles[i+dRight].autoColor.value.red * p/dRight : tiles[i+dRight].userColor.value.red * p/dRight)
-        //       + (tiles[i-dLeft].userColor.value == Colors.transparent ? tiles[i-dLeft].autoColor.value.red * p/dLeft : tiles[i-dLeft].userColor.value.red * p/dLeft);
-        // var G = (tiles[i-gridHeight*dUp].userColor.value == Colors.transparent ? tiles[i-gridHeight*dUp].autoColor.value.red * p/dUp : tiles[i-gridHeight*dUp].userColor.value.red * p/dUp)
-        //     + (tiles[i+gridHeight*dDown].userColor.value == Colors.transparent ? tiles[i+gridHeight*dDown].autoColor.value.red * p/dDown : tiles[i+gridHeight*dDown].userColor.value.red * p/dDown)
-        //     + (tiles[i+dRight].userColor.value == Colors.transparent ? tiles[i+dRight].autoColor.value.red * p/dRight : tiles[i+dRight].userColor.value.red * p/dRight)
-        //     + (tiles[i-dLeft].userColor.value == Colors.transparent ? tiles[i-dLeft].autoColor.value.red * p/dLeft : tiles[i-dLeft].userColor.value.red * p/dLeft);
-        // var B = (tiles[i-gridHeight*dUp].userColor.value == Colors.transparent ? tiles[i-gridHeight*dUp].autoColor.value.red * p/dUp : tiles[i-gridHeight*dUp].userColor.value.red * p/dUp)
-        //     + (tiles[i+gridHeight*dDown].userColor.value == Colors.transparent ? tiles[i+gridHeight*dDown].autoColor.value.red * p/dDown : tiles[i+gridHeight*dDown].userColor.value.red * p/dDown)
-        //     + (tiles[i+dRight].userColor.value == Colors.transparent ? tiles[i+dRight].autoColor.value.red * p/dRight : tiles[i+dRight].userColor.value.red * p/dRight)
-        //     + (tiles[i-dLeft].userColor.value == Colors.transparent ? tiles[i-dLeft].autoColor.value.red * p/dLeft : tiles[i-dLeft].userColor.value.red * p/dLeft);
-        int R = ((tiles[i-gridHeight*dUp].userColor.value.red * p/dUp)
-            + (tiles[i+gridHeight*dDown].userColor.value.red * p/dDown)
-            + (tiles[i+dRight].userColor.value.red * p/dRight)
-            + (tiles[i-dLeft].userColor.value.red * p/dLeft)).round();
-        int G = ((tiles[i-gridHeight*dUp].userColor.value.green * p/dUp)
-            + (tiles[i+gridHeight*dDown].userColor.value.green * p/dDown)
-            + (tiles[i+dRight].userColor.value.green * p/dRight)
-            + (tiles[i-dLeft].userColor.value.green * p/dLeft)).round();
-        int B = ((tiles[i-gridHeight*dUp].userColor.value.blue * p/dUp)
-            + (tiles[i+gridHeight*dDown].userColor.value.blue * p/dDown)
-            + (tiles[i+dRight].userColor.value.blue * p/dRight)
-            + (tiles[i-dLeft].userColor.value.blue * p/dLeft)).round();
-        tiles[i].autoColor.value = Color.fromRGBO(R, G, B, 1);
-      }
-    }
-    for (int i = 0; i < gridWidth * gridHeight; i++) {
-      if (tiles[i].userColor.value != Colors.transparent || tiles[i].autoColor.value != Colors.transparent) {
-        continue;
-      }
-      int dUp = distanceToClosestInclude(i,1);
-      int dDown = distanceToClosestInclude(i,2);
-      double p = 1/(1/dUp + 1/dDown);
-      if (dUp < gridHeight && dDown < gridHeight) {
-        Color upColor = tiles[i-gridHeight*dUp].userColor.value == Colors.transparent? tiles[i-gridHeight*dUp].autoColor.value : tiles[i-gridHeight*dUp].userColor.value;
-        Color downColor = tiles[i+gridHeight*dDown].userColor.value == Colors.transparent? tiles[i+gridHeight*dDown].autoColor.value : tiles[i+gridHeight*dDown].userColor.value;
-        int R = ((upColor.red * p/dUp)
-            + (downColor.red * p/dDown)).round();
-        int G = ((upColor.green * p/dUp)
-            + (downColor.green * p/dDown)).round();
-        int B = ((upColor.blue * p/dUp)
-            + (downColor.blue * p/dDown)).round();
-        tiles[i].autoColor.value = Color.fromRGBO(R, G, B, 1);
-      }
-    }
-    for (int i = 0; i < gridWidth * gridHeight; i++) {
-      if (tiles[i].userColor.value != Colors.transparent || tiles[i].autoColor.value != Colors.transparent) {
-        continue;
-      }
-      int dRight = distanceToClosestInclude(i,3);
-      int dLeft = distanceToClosestInclude(i,4);
-      double p = 1/(1/dRight + 1/dLeft);
-      if (dRight < gridHeight && dLeft < gridHeight) {
-        Color leftColor = tiles[i-dLeft].userColor.value == Colors.transparent? tiles[i-dLeft].autoColor.value : tiles[i-dLeft].userColor.value;
-        Color rightColor = tiles[i+dRight].userColor.value == Colors.transparent? tiles[i+dRight].autoColor.value : tiles[i+dRight].userColor.value;
-        p = 1/(1/dRight + 1/dLeft);
-        int R = ((rightColor.red * p/dRight)
-            + (leftColor.red * p/dLeft)).round();
-        int G = ((rightColor.green * p/dRight)
-            + (leftColor.green * p/dLeft)).round();
-        int B = ((rightColor.blue * p/dRight)
-            + (leftColor.blue * p/dLeft)).round();
-        tiles[i].autoColor.value = Color.fromRGBO(R, G, B, 1);
-
-      }
-    }
-
   }
+  void recalculate() {
+    if (userTiles.isEmpty) {
+      for (Tile tile in tiles) {
+        tile.autoColor.value = Colors.transparent;
+      }
+      return;
+    }
+    for (Tile tile in tiles) {
+      if (userTiles.contains(tile)) continue;
+      List<double> list = getDistanceToUserTiles(tile.index);
+      double p = 1/list.fold(0, (prev, e) => prev + 1/e);
+      double R = 0, G = 0, B = 0;
+      for (int i = 0; i < userTiles.length; i++) {
+        R += userTiles[i].userColor.value.red * p/list[i];
+        G += userTiles[i].userColor.value.green * p/list[i];
+        B += userTiles[i].userColor.value.blue * p/list[i];
+      }
 
-  // return values: up/down=1, neither=0, right/left=-1
-  // int closestDirectionUp(int index) {
-  //
-  //   return 1;
-  // }
-
-  // directions: up, down, right, left (1, 2, 3, 4)
-  int distanceToClosestInclude(int index, int direction) {
+      tile.autoColor.value = Color.fromRGBO(R.round(), G.round(), B.round(), 1);
+    }
+  }
+  List<double> getDistanceToUserTiles(int index) {
+    List <double> list = [];
     int row = index ~/ gridWidth;
     int col = index % gridWidth;
-    int distance = 1;
-    switch(direction) {
-      case 1:
-        for (int i = row - 1; i >= 0; i--) {
-          if (tiles[i*gridWidth + col].userColor.value != Colors.transparent || tiles[i*gridWidth + col].autoColor.value != Colors.transparent) return distance; // || tiles[i*gridWidth + col].autoColor.value != Colors.transparent
-          distance ++;
-        }
-        return gridHeight;
-      case 2:
-        for (int i = row + 1; i < gridHeight ; i++) {
-          if (tiles[i*gridWidth + col].userColor.value != Colors.transparent || tiles[i*gridWidth + col].autoColor.value != Colors.transparent) return distance; // || tiles[i*gridWidth + col].autoColor.value != Colors.transparent
-          distance++;
-        }
-        return gridHeight;
-      case 3:
-        for (int i = col + 1; i < gridWidth ; i++) {
-          if (tiles[row*gridWidth + i].userColor.value != Colors.transparent || tiles[row*gridWidth + i].autoColor.value != Colors.transparent) return distance; // || tiles[row*gridWidth + i].autoColor.value != Colors.transparent
-          distance++;
-        }
-        return gridWidth;
-      case 4:
-        for (int i = col - 1; i >=0 ; i--) {
-          if (tiles[row*gridWidth + i].userColor.value != Colors.transparent || tiles[row*gridWidth + i].autoColor.value != Colors.transparent) return distance; // || tiles[row*gridWidth + i].autoColor.value != Colors.transparent
-          distance++;
-        }
-        return gridWidth;
+    for (Tile userTile in userTiles) {
+      int curRow = userTile.index ~/ gridWidth;
+      int curCol = userTile.index % gridHeight;
+      list.add(sqrt(pow(curRow - row, 2) + pow(curCol - col, 2)));
     }
-    return 4;
-  }
-  int distanceToClosestExclude(int index, int direction) {
-    int row = index ~/ gridWidth;
-    int col = index % gridWidth;
-    int distance = 1;
-    switch(direction) {
-      case 1:
-        for (int i = row - 1; i >= 0; i--) {
-          if (tiles[i*gridWidth + col].userColor.value != Colors.transparent) return distance; // || tiles[i*gridWidth + col].autoColor.value != Colors.transparent
-          distance ++;
-        }
-        return gridHeight;
-      case 2:
-        for (int i = row + 1; i < gridHeight ; i++) {
-          if (tiles[i*gridWidth + col].userColor.value != Colors.transparent) return distance; // || tiles[i*gridWidth + col].autoColor.value != Colors.transparent
-          distance++;
-        }
-        return gridHeight;
-      case 3:
-        for (int i = col + 1; i < gridWidth ; i++) {
-          if (tiles[row*gridWidth + i].userColor.value != Colors.transparent) return distance; // || tiles[row*gridWidth + i].autoColor.value != Colors.transparent
-          distance++;
-        }
-        return gridWidth;
-      case 4:
-        for (int i = col - 1; i >=0 ; i--) {
-          if (tiles[row*gridWidth + i].userColor.value != Colors.transparent) return distance; // || tiles[row*gridWidth + i].autoColor.value != Colors.transparent
-          distance++;
-        }
-        return gridWidth;
-    }
-    return 4;
+    return list;
   }
 
   @override
@@ -209,12 +93,9 @@ class _MyAppState extends State<MyApp> {
         Tile(
           index: i,
           size: 100,
-          callback: setSelected,
+          setSelected: setSelected,
+          addToUserTiles: addToUserTiles,
         )
-    ];
-    dots = [
-      for (var i = 0; i < gridHeight*gridWidth; i++)
-        ValueNotifier(false)
     ];
     tilesWrapper = [
       for (var i = 0; i < gridHeight*gridWidth; i++)
@@ -257,7 +138,7 @@ class _MyAppState extends State<MyApp> {
                 tilesWrapper[newIndex] = temp2;
                 tiles[newIndex].index = newIndex;
                 tiles[oldIndex].index = oldIndex;
-                calculateGrid();
+                setSelected(newIndex);
                 setState(() {});
               },
             ),
@@ -272,15 +153,19 @@ class _MyAppState extends State<MyApp> {
                 border: const NeumorphicBorder(color: Colors.black12),
               ),
               child: ValueListenableBuilder<int>(
-                valueListenable: selected,
-                builder: (BuildContext context, int val, Widget? child) {
-                  return SizedBox(
-                    height: 255,
-                    width: 280,
-                    child: colorTile(val),
-                  );
-                }
+                  valueListenable: selected,
+                  builder: (BuildContext context, int val, Widget? child) {
+                    return SizedBox(
+                      height: 255,
+                      width: 280,
+                      child: colorTile(val),
+                    );
+                  }
               ),
+            ),
+            MaterialButton(
+              child: Text('Calculate Grid'),
+              onPressed: recalculate,
             ),
           ],
         ),
--- lib/tile.dart
+++ lib/tile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
-import 'tools/ValueListenableBuilder2.dart';
 import 'tools/ValueListenableBuilder3.dart';
 class Tile extends StatefulWidget {
   ValueNotifier<Color> userColor = ValueNotifier(Colors.transparent);
@@ -8,9 +7,11 @@ class Tile extends StatefulWidget {
   final double size;
   bool dot = false;
   int index;
-  final void Function(int) callback;
+  final void Function(int) setSelected;
+  final void Function(int, bool) addToUserTiles;
 
-  Tile({required this.index, required this.size, required this.callback, Key? key}) : super(key: key);
+
+  Tile({required this.index, required this.size, required this.setSelected, required this.addToUserTiles, Key? key}) : super(key: key);
 
   @override
   _TileState createState() => _TileState();
@@ -22,52 +23,55 @@ class _TileState extends State<Tile> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        widget.callback(widget.index);
+        widget.setSelected(widget.index);
         setState(() {});
       },
       child: Container(
         padding: const EdgeInsets.all(5),
         child: ValueListenableBuilder3<Color,Color, bool>(
-          first: widget.userColor,
-          second: widget.autoColor,
-          third: widget.selected,
+            first: widget.userColor,
+            second: widget.autoColor,
+            third: widget.selected,
             builder: (BuildContext context, Color color1, Color color2, bool value, Widget? child) {
-            if (color1 != Colors.transparent) {
-              widget.dot = true;
-            } else {
-              widget.dot = false;
-            }
-            return Neumorphic(
-              style: NeumorphicStyle(
-                shape: NeumorphicShape.flat,
-                boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
-                depth: 6,
-                lightSource: LightSource.topLeft,
-                intensity: .5,
-                color: color1 == Colors.transparent ? color2 == Colors.transparent ? const Color(0xFFD3D8DB) : color2 : color1,
-                border: NeumorphicBorder(width:  value ? 2 : .5, color: Colors.black12),
-              ),
-              padding: const EdgeInsets.all(2),
-              child: Container(
-                height: widget.size,
-                width: widget.size,
-                padding: const EdgeInsets.all(5),
-                // decoration: BoxDecoration(
-                //   borderRadius: BorderRadius.all(Radius.circular(widget.size / 10)),
-                //   color: widget.color,
-                //   border: Border.all(color: Colors.black12),
-                // ),
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: Icon(
-                    Icons.circle,
-                    size: 15,
-                    color: widget.dot ? value ? Colors.black54 : Colors.black12 : Colors.transparent,
+              if (color1 != Colors.transparent) {
+                if (!widget.dot) widget.addToUserTiles(widget.index, true);
+                widget.dot = true;
+              } else {
+                if (widget.dot) widget.addToUserTiles(widget.index, false);
+                widget.dot = false;
+
+              }
+              return Neumorphic(
+                style: NeumorphicStyle(
+                  shape: NeumorphicShape.flat,
+                  boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+                  depth: 6,
+                  lightSource: LightSource.topLeft,
+                  intensity: .5,
+                  color: color1 == Colors.transparent ? color2 == Colors.transparent ? const Color(0xFFD3D8DB) : color2 : color1,
+                  border: NeumorphicBorder(width:  value ? 2 : .5, color: Colors.black12),
+                ),
+                padding: const EdgeInsets.all(2),
+                child: Container(
+                  height: widget.size,
+                  width: widget.size,
+                  padding: const EdgeInsets.all(5),
+                  // decoration: BoxDecoration(
+                  //   borderRadius: BorderRadius.all(Radius.circular(widget.size / 10)),
+                  //   color: widget.color,
+                  //   border: Border.all(color: Colors.black12),
+                  // ),
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Icon(
+                      Icons.circle,
+                      size: 15,
+                      color: widget.dot ? value ? Colors.black54 : Colors.black12 : Colors.transparent,
+                    ),
                   ),
                 ),
-              ),
-            );
-          }
+              );
+            }
         ),
       ),
     );
